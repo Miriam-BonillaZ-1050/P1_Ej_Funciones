c4f282586164bb2154ae3d69c469b1eb8a060914
Miriam-BonillaZ-1050 <167555012+Miriam-BonillaZ-1050@users.noreply.github.com>
2025-02-07 16:47:40 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,18 @@
+// funcion que no devuelva valor
+// funcion saludar
+
+void saludar(){
+  print("Hola mundo!");
+}
+void escribeNombre(){
+  print("Miriam Bonilla");
+}
+void main(){
+  // "Miriam Bonilla, Mat: 22308051281050
+  // Llamar a la funcion saludar
+  print("Miriam Bonilla, Mat: 22308051281050");
+  print("Llamando a la funcion saludar");
+  saludar();
+  print("Llamando a la funcion escribeNombre");
+  escribeNombre();
+}
